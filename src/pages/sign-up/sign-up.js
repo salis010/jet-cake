349c48442320316fc76b0ed334ed99e67feff594
Stephen <stephen.saliba@gmail.com>
2020-01-28 20:31:16 +0100

    fixed breakpoint

--- src/pages/sign-up/sign-up.js
+++ src/pages/sign-up/sign-up.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react'
 import styled from '@emotion/styled'
-import { baseColor, maxWidth } from '../../theme'
+import { baseColor, breakpoint } from '../../theme'
 
 const Wrapper = styled.div`
     display: flex;
@@ -12,9 +12,10 @@ const Form = styled.form`
     grid-template-columns: 100%;
     grid-row-gap: 1em;
     grid-column-gap: 0;
+    width: 90%;
     margin: 20px 0px;
 
-    @media only screen and (min-width: 768px) {
+    @media only screen and (min-width: ${breakpoint}) {
         grid-template-columns: 15em 35em;
         grid-row-gap: 2em;
         margin: 40px 0px;
@@ -28,7 +29,7 @@ const Label = styled.label`
     margin-right: 0px;
     color: ${baseColor};
 
-    @media only screen and (min-width: 768px) {
+    @media only screen and (min-width: ${breakpoint}) {
         margin-right: 30px;
     }
 `
@@ -36,6 +37,7 @@ const Label = styled.label`
 const Input = styled.input`    
     align-self: center;
     height: 2.4em;
+    margin-bottom: 1em;
     font-size: 1.4em;
     text-indent: 1em;
     border: 3px solid grey;
@@ -50,14 +52,29 @@ const Input = styled.input`
     &[type=number] {
         -moz-appearance:textfield;
     }
+
+    @media only screen and (min-width: ${breakpoint}) {
+        margin-bottom: 0;
+    }
+`
+
+const Select = styled.select`
+    align-self: center;
+    height: 2.4em;
+    font-size: 1.4em;
+    text-indent: 1em;
+    border: 3px solid grey;
+    border-radius: 0.5em;
+    outline: none;
 `
 
 const Button = styled.button`
     grid-column-start: 1;
     grid-column-end: 2;
-    grid-row-start: 9;
-    grid-row-end: 10;
+    grid-row-start: 21;
+    grid-row-end: 22;
     height: 3.2em;
+    margin-top: 0.8em;
     font-size: 1.8em;
     font-weight: 800;
     color: white;
@@ -66,12 +83,12 @@ const Button = styled.button`
     border-radius: 9999px;
     outline: none;
 
-    @media only screen and (min-width: 768px) {
+    @media only screen and (min-width: ${breakpoint}) {
         margin-bottom: 0px;
         grid-column-start: 1;
     grid-column-end: 3;
-    grid-row-start: 5;
-    grid-row-end: 6;
+    grid-row-start: 11;
+    grid-row-end: 12;
     }
 `
 
@@ -96,6 +113,24 @@ export const SignUp = props => {
             
                 <Label>Date of Birth:</Label>
                 <Input type={dateType} onFocus={onDateFocus} onBlur={onDateBlur} />
+
+                <Label>Security Question 1:</Label>
+                <Select name=''></Select>
+
+                <Label>Answer 1:</Label>
+                <Input type='text' />
+
+                <Label>Security Question 2:</Label>
+                <Select name=''></Select>
+
+                <Label>Answer 2:</Label>
+                <Input type='text' />
+
+                <Label>Security Question 3:</Label>
+                <Select name=''></Select>
+
+                <Label>Answer 3:</Label>
+                <Input type='text' />
 
                 <Button>Sign Up</Button>
                 
