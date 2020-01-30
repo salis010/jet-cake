64d48d68cca520310d32cef50fb4ff3faa364607
Stephen <stephen.saliba@gmail.com>
2020-01-30 14:13:49 +0100

    sign-up validation complete

--- src/pages/sign-up/sign-up.js
+++ src/pages/sign-up/sign-up.js
@@ -4,6 +4,7 @@ import { baseColor, breakpoint } from '../../theme'
 import { FileInput } from './file-input'
 import { Label } from './label'
 import { securityQuestions } from './security-questions'
+import { validateForm } from './validate-form'
 
 const Wrapper = styled.div`
     display: flex;
@@ -27,6 +28,7 @@ const Form = styled.form`
     @media only screen and (min-width: ${breakpoint}) {
         grid-template-columns: 15rem 35rem;
         grid-row-gap: 2rem;
+        max-width: 60rem;
         margin: 40px 0px;
     }
 `
@@ -88,7 +90,7 @@ const Button = styled.input`
     grid-column-end: 2;
     grid-row-start: 21;
     grid-row-end: 22;
-    height: 3.2rem;
+    height: 4.2rem;
     margin-top: 0.8rem;
     font-size: 1.8rem;
     font-weight: 800;
@@ -111,17 +113,18 @@ const Button = styled.input`
 export const SignUp = props => {
 
     const [ formData, setFormData ] = useState({
-        photo: { value: '', isProcessed: false, isValid: false },
-        phoneNumber: { value: '', isProcessed: false, isValid: false },
-        address: { value: '', isProcessed: false, isValid: false },
-        email: { value: '', isProcessed: false, isValid: false },
-        dateOfBirth: { value: '', isProcessed: false, isValid: false },
-        question1: { value: '', isProcessed: false, isValid: false },
-        answer1: { value: '', isProcessed: false, isValid: false },
-        question2: { value: '', isProcessed: false, isValid: false },
-        answer2: { value: '', isProcessed: false, isValid: false },
-        question3: { value: '', isProcessed: false, isValid: false },
-        answer3: { value: '', isProcessed: false, isValid: false },
+        formReviewed: false,
+        photo: { value: '', isValid: false },
+        phoneNumber: { value: '', isValid: false },
+        address: { value: '', isValid: false },
+        email: { value: '', isValid: false },
+        dateOfBirth: { value: '', isValid: false },
+        question1: { value: '', isValid: false },
+        answer1: { value: '', isValid: false },
+        question2: { value: '', isValid: false },
+        answer2: { value: '', isValid: false },
+        question3: { value: '', isValid: false },
+        answer3: { value: '', isValid: false },
     })
 
     // removes the date placeholder 'DD/MM/YYYY' from the Date field
@@ -130,18 +133,25 @@ export const SignUp = props => {
     const onDateBlur = () => setDateType('text')
 
     const handleChange = event => {
-        setFormData({...formData, [event.target.name]: { value: event.target.value, isProcessed: false, isValid: false }})
+        setFormData({...formData, [event.target.name]: { value: event.target.value, isValid: false }})
     }
 
     const handleFileChange = event => {
         const file = event.target.files[0]
       
-        setFormData({...formData, [event.target.name]: { value: file, isProcessed: false, isValid: false } })
+        setFormData({...formData, [event.target.name]: { value: file, isValid: false } })
     }
 
     const handleSubmit = event => {
-        console.log(formData)
+        
         event.preventDefault()        
+
+        if(validateForm(formData, setFormData)) {
+            // submit data to server
+            // redirect to homepage
+        } else {
+            // validateForm will return a new State object which will automatically show the failing fields
+        }
     }
 
     
@@ -151,37 +161,37 @@ export const SignUp = props => {
             <Photo src={formData.photo.value && URL.createObjectURL(formData.photo.value)} isVisible={formData.photo.value} />
             <Form onSubmit={handleSubmit} >
 
-                <Label name='Photo' isProcessed={formData.photo.isProcessed} isValid={formData.photo.isValid} />
+                <Label name='Photo' formReviewed={formData.formReviewed} isValid={formData.photo.isValid} />
                 <FileInput name='photo' onChange={handleFileChange} />
 
-                <Label name='phoneNumber' isProcessed={formData.phoneNumber.isProcessed} isValid={formData.phoneNumber.isValid} />
+                <Label name='Phone Number' formReviewed={formData.formReviewed} isValid={formData.phoneNumber.isValid} />
                 <Input type='number' name='phoneNumber' value={formData.phoneNumber.value} onChange={handleChange} />            
             
-                <Label name='address' isProcessed={formData.address.isProcessed} isValid={formData.address.isValid} />
+                <Label name='Address' formReviewed={formData.formReviewed} isValid={formData.address.isValid} />
                 <Input type='text' name='address' value={formData.address.value} onChange={handleChange} />
             
-                <Label name='email' isProcessed={formData.email.isProcessed} isValid={formData.email.isValid} />
+                <Label name='email' formReviewed={formData.formReviewed} isValid={formData.email.isValid} />
                 <Input type='email' name='email' value={formData.email.value} onChange={handleChange} />
             
-                <Label name='dateOfBirth' isProcessed={formData.dateOfBirth.isProcessed} isValid={formData.dateOfBirth.isValid} />
+                <Label name='Date of Birth' formReviewed={formData.formReviewed} isValid={formData.dateOfBirth.isValid} />
                 <Input type={dateType} name='dateOfBirth' value={formData.dateOfBirth.value} onFocus={onDateFocus} onBlur={onDateBlur} onChange={handleChange} />
 
-                <Label name='question1' isProcessed={formData.question1.isProcessed} isValid={formData.question1.isValid} />
+                <Label name='Security Question 1' formReviewed={formData.formReviewed} isValid={formData.question1.isValid} />
                 <Select name='question1' defaultValue={'default'} onChange={handleChange} >{options}</Select>
 
-                <Label name='answer1' isProcessed={formData.answer1.isProcessed} isValid={formData.answer1.isValid} />
+                <Label name='Answer 1' formReviewed={formData.formReviewed} isValid={formData.answer1.isValid} />
                 <Input type='text' name='answer1' value={formData.answer1.value} onChange={handleChange} />
 
-                <Label name='question2' isProcessed={formData.question2.isProcessed} isValid={formData.question2.isValid} />
+                <Label name='Security Question 2' formReviewed={formData.formReviewed} isValid={formData.question2.isValid} />
                 <Select name='question2' defaultValue={'default'} onChange={handleChange} >{options}</Select>
 
-                <Label name='answer2' isProcessed={formData.answer2.isProcessed} isValid={formData.answer2.isValid} />
+                <Label name='Answer 2' formReviewed={formData.formReviewed} isValid={formData.answer2.isValid} />
                 <Input type='text' name='answer2' value={formData.answer2.value} onChange={handleChange} />
 
-                <Label name='question3' isProcessed={formData.question3.isProcessed} isValid={formData.question3.isValid} />
+                <Label name='Security Question 3' formReviewed={formData.formReviewed} isValid={formData.question3.isValid} />
                 <Select name='question3' defaultValue={'default'} onChange={handleChange}>{options}</Select>
 
-                <Label name='answer3' isProcessed={formData.answer3.isProcessed} isValid={formData.answer3.isValid} />
+                <Label name='Answer 3' formReviewed={formData.formReviewed} isValid={formData.answer3.isValid} />
                 <Input type='text' name='answer3' value={formData.answer3.value} onChange={handleChange} />
 
                 <Button type='submit' value='Sign Up' onChange={handleChange} />
